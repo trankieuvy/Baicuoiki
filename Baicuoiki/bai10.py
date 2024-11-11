53b699d013630b97d4d9fe23ffd75fe5f8a8eaa3
KhanhTrang005 <158994675+KhanhTrang005@users.noreply.github.com>
2024-11-11 21:43:04 +0700

    Update bai10.py

--- Baicuoiki/bai10.py
+++ Baicuoiki/bai10.py
@@ -6,11 +6,12 @@ Created on Mon Nov  4 21:44:23 2024
 """
 
 #10
-def question_10():
+def question_10() -> None:
     ds = input("Nhập danh sách: ")
     tach = ds.split()
     if tach:
-        return tach
+        return ''.join([i for i in tach])
     return None
+
 if __name__ == "__main__":
      print(question_10())
