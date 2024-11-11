a0f4b54f201a2417b0e1866ae6feff3f02779afc
KhanhTrang005 <158994675+KhanhTrang005@users.noreply.github.com>
2024-11-11 21:32:51 +0700

    Update bai9.py

--- Baicuoiki/bai9.py
+++ Baicuoiki/bai9.py
@@ -4,10 +4,13 @@ Created on Mon Nov  4 21:03:19 2024
 
 @author: Windows
 """
-#9
-def question_9(s):
-    return s == s[::-1]
+def question_9(s: str) -> bool:
+    s = s.lower()
+    chuoi = [i for i in s if i.isalnum()]
+    return chuoi == chuoi[::-1]
  
 if __name__ == "__main__":
      print(question_9("madam"))
+     print(question_9("race a car"))
+     print(question_9("A man, a plan, a canal: Panama"))
     
