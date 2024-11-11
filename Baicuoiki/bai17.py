56a1efb22f458406ac869b8cc008bafd64a38310
KhanhTrang005 <158994675+KhanhTrang005@users.noreply.github.com>
2024-11-11 22:08:40 +0700

    Update bai17.py

--- Baicuoiki/bai17.py
+++ Baicuoiki/bai17.py
@@ -6,11 +6,8 @@ Created on Fri Nov  8 21:44:57 2024
 """
 import random
 def question_17(n: int) -> list:
-    ds = []
-    for i in range(n):
-        so = random.randint(1,100)
-        ds.append(so)
-    return sorted(ds, reverse= False)
+    ds = [random.randint(1,100) for i in range(n)]
+    return sorted(ds, reverse = True)
 
 if __name__ == "__main__":
     print(question_17(8))
