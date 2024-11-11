2156eb0121f04d83d908204006ad621e70f00bcb
KhanhTrang005 <158994675+KhanhTrang005@users.noreply.github.com>
2024-11-11 21:55:53 +0700

    Update bai14.py

--- Baicuoiki/bai14.py
+++ Baicuoiki/bai14.py
@@ -6,9 +6,9 @@ Created on Mon Nov  4 22:09:09 2024
 """
 
 #14
-def question_14(s):
-    if s.isdigit():
-        return True
-    return False
+def question_14(s: str) -> bool:
+    return s.isdigit()
+
 if __name__ == "__main__":
      print(question_14("123"))
+     print(question_14("123a45"))
