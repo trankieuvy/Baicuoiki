e933ca6fd4201d2696793fb0908b95e91b3c8355
trankieuvy <trankieuvy2005@gmail.com>
2024-11-11 17:43:53 +0700

    Update bai11+bai12.py

--- Baicuoiki/bai11.py
+++ Baicuoiki/bai11.py
@@ -4,6 +4,25 @@ Created on Mon Nov  4 21:54:25 2024
 
 @author: Windows
 """
+#11
+def question_11(n: int) -> int:
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    sotrc = 0
+    sosau = 1
+    
+    for i in range(2,n+1):
+        sohientai = sotrc + sosau
+        sotrc=sosau
+        sosau=sohientai
+    return sohientai
+if __name__=="__main__":
+    print(question_11(5))
+    print(question_11(10))
+
+
 import random
 #12
 def question_12():
