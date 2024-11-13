fb8a66f5b34f82f87fab315155a6bca0b104d646
KhanhTrang005 <158994675+KhanhTrang005@users.noreply.github.com>
2024-11-13 20:14:20 +0700

    Add files via upload

---
+++ Baicuoiki/bai39.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 19:40:06 2024
+
+@author: Windows
+"""
+def question_39(prices: list[int]) -> int:
+     min_prices = prices[0]
+     max_prices = 0
+     for i in range(1, len(prices)):
+         gia_hien_tai = prices[i]- min_prices
+         max_prices = max(max_prices, gia_hien_tai)
+         min_prices = min(min_prices, prices[i])
+     return max_prices 
+if __name__ == "__main__":
+    print(question_39([6, 7, 8, 9, 20, 5]))   
+    print(question_39([7, 1, 5, 3, 6, 4]))   
+    print(question_39([7, 6, 4, 3, 1]))
+         
+         
+          
+
