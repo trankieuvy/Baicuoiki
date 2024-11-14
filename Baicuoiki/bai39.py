5208b04f1acb6061448bf3233264196771923ed7
KhanhTrang005 <158994675+KhanhTrang005@users.noreply.github.com>
2024-11-14 21:09:36 +0700

    Update bai39.py

--- Baicuoiki/bai39.py
+++ Baicuoiki/bai39.py
@@ -5,13 +5,22 @@ Created on Wed Nov 13 19:40:06 2024
 @author: Windows
 """
 def question_39(prices: list[int]) -> int:
+     #cach 1
      min_prices = prices[0]
      max_prices = 0
      for i in range(1, len(prices)):
          gia_hien_tai = prices[i]- min_prices
          max_prices = max(max_prices, gia_hien_tai)
          min_prices = min(min_prices, prices[i])
-     return max_prices 
+     #cach 2
+    min_prices = 10**9
+    max_prices =0
+    for i in prices:
+        if i < min_prices:
+            min_prices = i
+        elif i - min_prices> max_prices:
+            max_prices = i-min_prices
+    return max_prices
 if __name__ == "__main__":
     print(question_39([6, 7, 8, 9, 20, 5]))   
     print(question_39([7, 1, 5, 3, 6, 4]))   
