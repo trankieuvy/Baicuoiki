666d8a3462148221fb08e8de008de808715969c5
trankieuvy <trankieuvy2005@gmail.com>
2024-11-14 19:36:26 +0700

    Update bai39.py

--- Baicuoiki/bai39.py
+++ Baicuoiki/bai39.py
@@ -16,7 +16,23 @@ if __name__ == "__main__":
     print(question_39([6, 7, 8, 9, 20, 5]))   
     print(question_39([7, 1, 5, 3, 6, 4]))   
     print(question_39([7, 6, 4, 3, 1]))
-         
+
+#Cách 2
+def question_39(prices: list[int]) -> int:
+    min_prices = 10**9
+    max_prices =0
+    for i in prices:
+        if i < min_prices:
+            min_prices = i
+        elif i - min_prices> max_prices:
+            max_prices = i-min_prices
+    return max_prices
+if __name__=="__main__":
+    print(question_39([6, 7, 8, 9, 20, 5]))
+    print(question_39([7, 1, 5, 3, 6, 4]))
+    print(question_39([7, 6, 4, 3, 1]))
+
+            
          
           
 
